60184ac0ce0e8df2637219c2c4f9d724c5ffe232
Soy programador <soytecnologicomx@gmail.com>
2024-12-17 04:02:16 +0000

    Seeding data

--- apirest/models/category.py
+++ apirest/models/category.py
@@ -8,12 +8,12 @@ class Category(models.Model):
     """
 
     class CategoryType(models.TextChoices):
-        INCOME = 'IN', 'Income'
-        EXPENSE = 'EX', 'Expense'
+        INCOME = 'income', 'Income'
+        EXPENSE = 'expense', 'Expense'
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.TextField(max_length=255, null=False, blank=False)
-    type = models.CharField(max_length=2, choices=CategoryType.choices, null=False, blank=False)
+    type = models.CharField(max_length=20, choices=CategoryType.choices, null=False, blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
--- apirest/models/merchant.py
+++ apirest/models/merchant.py
@@ -10,12 +10,12 @@ class Merchant(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     merchant_name = models.CharField(max_length=255, null=False, blank=False)
     merchant_logo = models.CharField(max_length=255, null=True, blank=False)
-    category = models.OneToOneField(Category, 
+    category_id = models.ForeignKey(Category, 
                                     on_delete=models.RESTRICT,
-                                    null=False, 
+                                    null=True, 
                                     blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.name
+        return self.merchant_name
