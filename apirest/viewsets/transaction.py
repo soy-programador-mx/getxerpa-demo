3b517e3f41c5197fb5a8e54fb7c6d9bec2e352a7
Soy programador <soytecnologicomx@gmail.com>
2024-12-17 23:05:31 +0000

    List instead of Dic where create transactions

--- apirest/viewsets/transaction.py
+++ apirest/viewsets/transaction.py
@@ -1,6 +1,7 @@
 from ..serializers.transaction import TransactionSerializer
 from ..models import Transaction
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status
+from rest_framework.response import Response
 
 class TransactionViewSet(viewsets.ModelViewSet):
     queryset = Transaction.objects.all()
@@ -8,3 +9,10 @@ class TransactionViewSet(viewsets.ModelViewSet):
         permissions.AllowAny
     ]
     serializer_class = TransactionSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data, many=isinstance(request.data,list))
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
