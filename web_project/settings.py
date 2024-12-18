9830723958831dac0bfd23a2cf979de130142bea
Soy programador <soytecnologicomx@gmail.com>
2024-12-18 17:27:47 +0000

    Allowed host

--- web_project/settings.py
+++ web_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-+i@$&3merbkow(rck#_66r)-+7h1(n@5=bqep9%@f2%+g7)4dl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
