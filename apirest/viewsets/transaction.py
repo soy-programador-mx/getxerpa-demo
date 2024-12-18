2e8f05c0e6ac20debc43d6aee1be8942fad15c90
Soy programador <soytecnologicomx@gmail.com>
2024-12-18 16:30:40 +0000

    enrichment rates

--- apirest/viewsets/transaction.py
+++ apirest/viewsets/transaction.py
@@ -2,7 +2,6 @@
 from rest_framework import viewsets, permissions, status
 from rest_framework.response import Response
 from ..serializers.transaction import TransactionSerializer
-from django.core import serializers
 
 from ..models import Transaction
 
@@ -13,16 +12,36 @@ class TransactionViewSet(viewsets.ModelViewSet):
     ]
     serializer_class = TransactionSerializer
 
+    def rates_enrichment(self, data, rows_total):
+
+        category_enrichment = 0
+        merchant_enrichment = 0
+
+
+        for row in data:
+            if not row["enrichment"]["category"]:
+                category_enrichment += 1
+            if not row["enrichment"]["merchant"]:
+                merchant_enrichment += 1
+
+        return {
+            'category': (category_enrichment * 100) / rows_total,
+            'merchant': (merchant_enrichment * 100) / rows_total
+        }
+
     def create(self, request, *args, **kwargs):
 
         serializer = self.get_serializer(data=request.data, many=isinstance(request.data,list))
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
 
+        rates = self.rates_enrichment(serializer.data, len(serializer.data))
         response_data = {
-            'message': 'Transaction created successfully',
+            'transactions_total': len(serializer.data),
+            'category_enrichment_rate': rates["category"],
+            'merchant_enrichment_rate': rates["merchant"],
             'data': serializer.data
         }
         
-        headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        headers = self.get_success_headers(response_data)
+        return Response(response_data, status=status.HTTP_201_CREATED, headers=headers)
